77ba0fdd5c4158b39ac0904a2e0e96fc481fa3d1
maheshgoud543 <goudsmahesh123@gmail.com>
2024-02-28 14:46:40 +0530

    Adding Error Messages

--- register/src/App.jsx
+++ register/src/App.jsx
@@ -15,35 +15,49 @@ function App() {
     name: "username",
     type: "text",
     placeholder: "Enter your Name",
-    label: "UserName"
+    errormessage: "Username should be between 3-16 characters and Should not include any special Characters",
+    label: "UserName",
+    pattern: "John",
+    pattern: "^[A-Za-z0-9]{3,16}$",
+    required: true
   },
   {
     id: 2,
     name: "email",
-    type: "text",
+    type: "email",
     placeholder: "Enter your email",
-    label: "Email"
+    errormessage: "It should be valid Email!",
+    label: "Email",
+    required: true
   },
   {
     id: 3,
     name: "birthday",
-    type: "birthday",
+    type: "date",
     placeholder: "Enter your Birthday",
-    label: "BirthDay"
+    label: "BirthDay",
+
+
   },
   {
     id: 4,
     name: "password",
     type: "password",
     placeholder: "Enter your Password",
-    label: "Password"
+    errormessage: "Password should be 8-20 characters and should include atleast 1 letter, 1 number and 1 special character",
+    label: "Password",
+    pattern:"^(?=.*[0-9])(?=.*[a-zA-z])(?=.*[!@#$%^&*])[a-zA-z0-9!@#$%^&*]{8,20}$",
+    required: true
   },
   {
     id: 5,
     name: "confirmpassword",
     type: "password",
     placeholder: "Confirm your Password",
-    label: "Password"
+    errormessage: "passowords font match",
+    label: "Password",
+    pattern: values.password,
+    required: true
   },
   ]
   // const usernameRef = useRef()
