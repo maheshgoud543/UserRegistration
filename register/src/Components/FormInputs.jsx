745cb44201bd63e3b01688f1ed18784a3a3628e5
maheshgoud543 <goudsmahesh123@gmail.com>
2024-02-28 14:58:18 +0530

    Completed Form

--- register/src/Components/FormInputs.jsx
+++ register/src/Components/FormInputs.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./FormInput.css"
 const FormInput = (props) => {
-    const { label,errormessage, id, onChange, ...inputProps } = props;
+    const [focused, setFocused] = useState(false)
+    const { label, errormessage, id, onChange, ...inputProps } = props;
+
+    const handlefocused = (e) => {
+        setFocused(true);
+    }
+
+
     return (
         <div className="form_inputs">
             <label>{label}</label>
-            <input {...inputProps} onChange={onChange} />
+            <input {...inputProps} onChange={onChange} onBlur={handlefocused} onFocus={() => {
+                inputProps.name === "confirmpassword" && setFocused(true)
+            }} focused={focused.toString() } />
             <span>{errormessage}</span>
         </div>
     )
